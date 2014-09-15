1ecbfcb504fc46a7eb4338412f632323273b9394
no bones about it <chronogene@gmail.com>
2014-09-15 09:43:17 -0700

    creating it for reals

---
+++ include/mymaths.h
@@ -0,0 +1,202 @@
+class Vector2
+{
+private:
+	float x;
+	float y;
+public:
+	Vector2();
+	Vector2(float, float);
+	void Set(float ax, float ay)
+	{
+		x = ax;
+		y = ay;
+	}
+	float GetX()
+	{
+		return x;
+	}
+	float GetY()
+	{
+		return y;
+	}
+};
+
+Vector2::Vector2(float ax, float ay)
+{
+	x = ax;
+	y = ay;
+}
+
+class Point2D
+{
+private:
+	float x;
+	float y;
+public:
+	Point2D();
+	Point2D(float, float);
+	void Set(float ax, float ay)
+	{
+		x = ax;
+		y = ay;
+	}
+	float GetX()
+	{
+		return x;
+	}
+	float GetY()
+	{
+		return y;
+	}
+};
+Point2D::Point2D(float ax, float ay)
+{
+	x = ax;
+	y = ay;
+}
+
+class Line2D
+{
+private:
+	float X1;
+	float Y1;
+	float X2;
+	float Y2;
+public:
+	void Set(float startx, float starty, float endx, float endy)
+	{
+		X1 = startx;
+		Y1 = starty;
+		X2 = endx;
+		Y2 = endy;
+	}
+	Line2D();
+	Line2D(Point2D, Point2D);
+	void Set(Point2D a_point1, Point2D a_point2)
+	{
+		X1 = a_point1.GetX();
+		Y1 = a_point1.GetY();
+		X2 = a_point2.GetX();
+		Y2 = a_point2.GetY();
+	}
+	float GetLength()
+	{
+		return (((X2 - X1) * (X2 - X1)) + ((Y2 - Y1) * (Y2 - Y1)));
+	}
+	float GetLengthX()
+	{
+		return (X2 - X1);
+	}
+	float GetLengthY()
+	{
+		return (Y2 - Y1);
+	}
+	Vector2 GetVector()
+	{
+		Vector2 v(GetLengthX(),GetLengthY());
+		return v;
+	}
+};
+
+Line2D::Line2D(Point2D a_point1, Point2D a_point2)
+{
+	X1 = a_point1.GetX();
+	Y1 = a_point1.GetY();
+	X2 = a_point2.GetX();
+	Y2 = a_point2.GetY();
+}
+
+
+class Box2D
+{
+private:
+	float X1;
+	float Y1;
+	float X2;
+	float Y2;
+public:
+	Box2D(void);
+	Box2D(Point2D, Point2D);
+	Box2D(float, float, float, float);
+	void Set(Point2D p1, Point2D p2)
+	{
+		X1 = p1.GetX();
+		Y1 = p1.GetY();
+		X2 = p2.GetX();
+		Y2 = p2.GetY();
+	}
+	Point2D GetLeftBot()
+	{
+		Point2D p(X1,Y1);
+		return p;
+	}
+	Point2D GetLeftTop()
+	{
+		Point2D p(X1, Y2);
+		return p;
+	}
+	Point2D GetRightBot()
+	{
+		Point2D p(X2, Y1);
+		return p;
+	}
+	Point2D GetRightTop()
+	{
+		Point2D p(X2, Y2);
+		return p;
+	}
+	Line2D GetBotLine()
+	{
+		Line2D l(GetLeftBot(),GetRightBot());
+		return l;
+	}
+	Line2D GetRightLine()
+	{
+		Line2D l(GetRightBot(), GetRightTop());
+		return l;
+	}
+	Line2D GetTopLine()
+	{
+		Line2D l(GetRightTop(), GetLeftTop());
+		return l;
+	}
+	Line2D GetLeftLine()
+	{
+		Line2D l(GetLeftTop(), GetLeftBot());
+		return l;
+	}
+};
+
+Box2D::Box2D(Point2D p1, Point2D p2)
+{
+	X1 = p1.GetX();
+	Y1 = p1.GetY();
+	X2 = p2.GetX();
+	Y2 = p2.GetY();
+}
+Box2D::Box2D(float ax1, float ay1, float ax2, float ay2)
+{
+	X1 = ax1;
+	Y1 = ay1;
+	X2 = ax2;
+	Y2 = ay2;
+}
+
+float Dot(Vector2 a, Vector2 b)
+{
+	return ((a.GetX() * b.GetX()) + (a.GetY() * b.GetY()));
+}
+
+bool CheckPointBox(Point2D p, Box2D b)
+{
+	if (p.GetX() > b.GetLeftBot().GetX() && p.GetX() < b.GetRightTop().GetX() && p.GetY() > b.GetLeftBot().GetY() && p.GetY() < b.GetRightTop().GetY())
+		return true;
+	return false;
+}
+
+bool CheckBoxBox(Box2D b1, Box2D b2)
+{
+	if (CheckPointBox(b1.GetLeftBot(), b2) || CheckPointBox(b1.GetRightBot(), b2) || CheckPointBox(b1.GetRightTop(), b2) || CheckPointBox(b1.GetLeftTop(), b2))
+		return true;
+	return false;
+}
---
+++ source/main.cpp
@@ -0,0 +1,337 @@
+﻿#include "AIE.h"
+#include <iostream>
+#include <math.h>
+#include <MyMaths.h>
+#include <cmath>
+
+
+const int iScreenWidth = 672;		//GLOBALS
+const int iScreenHeight = 780;
+const char* gameTitle = "spess invadur";
+const char* cScore1;
+const char* cScore2;
+unsigned int iArcadeMarquee;
+float fDeltaT;
+const char* pInvadersFont = "./fonts/invaders.fnt";
+float fKeyPastOne = 0, fKeyPastTwo = 0;
+
+void Load();
+void UpdateGameState();
+void UpdateMainMenu();
+
+bool LineCircleCheck(float d, float f, float r)
+{
+
+	return true;
+}
+struct PlayerCannon					//CREATING A PLAYER
+{
+	unsigned int iSpriteID;
+	float Width;
+	float Height;
+	float x;
+	float y;
+	void SetSize(float a_fWidth, float a_fHeight)
+	{
+		Width = a_fWidth;
+		Height = a_fHeight;
+	}
+
+	void SetPosition(float a_x, float a_y)
+	{
+		x = a_x;
+		y = a_y;
+	}
+
+	unsigned int iMoveLeftKey, iMoveRightKey;
+	void SetMovementKeys(unsigned int a_moveleft, unsigned int a_moveRight)
+	{
+		iMoveLeftKey = a_moveleft;
+		iMoveRightKey = a_moveRight;
+	}
+
+	unsigned int iLeftMovementExtreme, iRightMovementExtreme;
+	void SetMovementExtremes(unsigned int a_leftExtreme, unsigned int a_rightExtreme)
+	{
+		iLeftMovementExtreme = a_leftExtreme;
+		iRightMovementExtreme = a_rightExtreme;
+	}
+	void Move(float a_fTimeStep, float a_fSpeed)
+	{
+
+		/*if (IsKeyDown(iMoveLeftKey) && IsKeyDown(iMoveRightKey))
+		{
+			if (fKeyPastOne = 1 && fKeyPastTwo == 0)
+			{
+				x -= a_fTimeStep * a_fSpeed;
+				if (x > (iRightMovementExtreme - fWidth*.5f))
+				{
+					x = (iRightMovementExtreme - fWidth*.5f);
+				}
+			}
+			else
+			{
+				x -= a_fTimeStep * a_fSpeed;
+				if (x < (iLeftMovementExtreme + fWidth*.5f))
+				{
+					x = (iLeftMovementExtreme + fWidth*.5f);
+				}
+			}
+		}
+		else
+		{
+			if (IsKeyDown(iMoveLeftKey))
+			{
+				fKeyPastOne = 1;
+				x -= a_fTimeStep * a_fSpeed;
+				if (x < (iLeftMovementExtreme + fWidth*.5f))
+				{
+					x = (iLeftMovementExtreme + fWidth*.5f);
+				}
+			}
+			else
+				fKeyPastOne = 0;
+			if (IsKeyDown(iMoveRightKey))
+			{
+				fKeyPastTwo = 1;
+				x += a_fTimeStep * a_fSpeed;
+				if (x > (iRightMovementExtreme - fWidth*.5f))
+				{
+					x = (iRightMovementExtreme - fWidth*.5f);
+				}
+			}
+			else
+				fKeyPastTwo = 0;
+		}*/
+		if (IsKeyDown(iMoveLeftKey))
+		{
+			x -= a_fTimeStep * a_fSpeed;
+			if (x < (iLeftMovementExtreme + Width*.5f))
+			{
+				x = (iLeftMovementExtreme + Width*.5f);
+			}
+		}
+		if (IsKeyDown(iMoveRightKey))
+		{
+			x += a_fTimeStep * a_fSpeed;
+			if (x >(iRightMovementExtreme - Width*.5f))
+			{
+				x = (iRightMovementExtreme - Width*.5f);
+			}
+		}
+		MoveSprite(iSpriteID, x, y);
+	}
+	Box2D GetBox2D()
+	{
+		Box2D b(x - (.5f * Width), y - (.5f * Height), x + Width, y + Height);
+		//Box2D b(5, 5, 15, 15);
+		//return Box2D(5,5,15,15);
+		return b;
+	}
+};
+PlayerCannon player;
+
+char pkScore1[10] = "00000";		//MORE GLOBALS
+char pkHighScore[10] = "00000";
+char pkScore2[10] = "00000";
+char pkCredit[10] = "99";
+
+enum GAMESTATES
+{
+	eMAIN_MENU,
+	eGAMEPLAY,
+	eEND
+};
+GAMESTATES eCurrentState = eMAIN_MENU;
+
+int FUCK(int FUCKING_NOTHING)		//GOD DAMNIT
+{
+	FUCKING_NOTHING = 0;
+	return FUCKING_NOTHING;
+}
+struct Enemy						//I AM AN ENEMY
+{
+private:
+	float fWidth, fHeight, x, y;
+	unsigned int iLeftMovementExtreme, iRightMovementExtreme;
+public:
+	bool alive;
+	unsigned int iSpriteID;
+	float iDirection;
+
+	void SetLoc(float a_x, float a_y)
+	{
+		x = a_x;
+		y = a_y;
+	}
+	float GetX()
+	{
+		return x;
+	}
+	float GetY()
+	{
+		return y;
+	}
+	void SetMovementExtremes(unsigned int a_leftExtreme, unsigned int a_rightExtreme)
+	{
+		iLeftMovementExtreme = a_leftExtreme;
+		iRightMovementExtreme = a_rightExtreme;
+	}
+	void GoForward(float a_DeltaT)
+	{
+		x += (a_DeltaT * iDirection * iScreenWidth);
+	}
+	bool SwitchCheck()
+	{
+		if (x < iLeftMovementExtreme || x > iRightMovementExtreme)
+			return true;
+		else
+			return false;
+	}
+	Box2D GetBox2D()
+	{
+		//Box2D b(x, y, x + fWidth, y + fHeight);											//FIX ME TOO
+		Box2D b(0, 0, 200, 200);
+		return b;
+	}
+};
+Enemy Aliens[15];
+
+void CreateEnemies()				//CREATING THE ENEMIES
+{
+	float fEnemyX = iScreenWidth * 0.2f;
+	float fEnemyY = iScreenHeight * 0.8f;
+	float fTempLeft = 0, fTempRight = 0;
+	for (int i = 0; i < 15; ++i)
+	{
+		Aliens[i].SetMovementExtremes(67 + fTempLeft, 288 + fTempRight);
+		Aliens[i].iSpriteID = CreateSprite("./images/invaders/invaders_1_00.png", 32, 27, true);
+		Aliens[i].iDirection = .05f;
+		Aliens[i].SetLoc(fEnemyX, fEnemyY);
+		Aliens[i].alive = true;
+		MoveSprite(Aliens[i].iSpriteID, Aliens[i].GetX(), Aliens[i].GetY());
+		fEnemyX += 0.12f * iScreenWidth;
+		fTempLeft += 0.12 * iScreenWidth;
+		fTempRight += 0.12 * iScreenWidth;
+		if (fEnemyX >= iScreenWidth * 0.8f)
+		{
+			fTempLeft = 0;
+			fTempRight = 0;
+			fEnemyX = iScreenWidth * 0.2f;
+			fEnemyY = fEnemyY - 30;
+		}
+	}
+}
+
+
+int main( int argc, char* argv[] )
+{	
+    Initialise(iScreenWidth, iScreenHeight, false, gameTitle);
+	Load();
+	AddFont(pInvadersFont);
+    do
+    {
+		ClearScreen();
+		fDeltaT = GetDeltaTime();
+		//END STATE CHANGE
+		if (IsKeyDown(GLFW_KEY_ESCAPE))
+			eCurrentState = eEND;
+		//SET FONT
+		SetFont(pInvadersFont);
+		switch (eCurrentState)
+		{
+		case eMAIN_MENU:
+			UpdateMainMenu();
+			break;
+		case eGAMEPLAY:
+			UpdateGameState();
+			break;
+		case eEND:
+			return 0;
+		default:
+			break;
+		}
+    } while(!FrameworkUpdate());
+
+    Shutdown();
+
+    return 0;
+}
+
+void Load()
+{
+    SetBackgroundColour(SColour(0, 40, 0, 255));
+	iArcadeMarquee = CreateSprite("./images/marq.png", 750, 800, true);
+	MoveSprite(iArcadeMarquee, 330, 380);
+	player.SetSize(64.f, 32.f);
+	player.SetMovementKeys('Z', 'X');
+	player.iSpriteID = CreateSprite("./images/cannon.png",player.Width,player.Height,true);
+	player.x = iScreenWidth * 0.5f;
+	player.y = 80.f;
+	player.SetMovementExtremes(32, 640);
+	/*Point2D point(5.f, 5.f);
+	Box2D box(0.f, 0.f, 10.f, 10.f);
+	Box2D box2(5.f, 5.f, 15.f, 15.f);
+	if (CheckBoxBox(box, box2) && CheckPointBox(point,box))
+	{
+		eCurrentState = eEND;
+	}*/
+};
+
+void UpdateMainMenu()
+{
+	DrawString("Insert Coins", iScreenWidth*0.37f, iScreenHeight * 0.5f);
+	DrawString("Credits", iScreenWidth * 0.41f, iScreenHeight * 0.4f);
+	for (int a_i = 250; a_i < 450; a_i = a_i + 2)
+	{
+		DrawLine(200, a_i, 450, a_i, SColour(0x00, 0x00, 0x00, 0xFF)); //SCANLINES
+	}
+			if (IsKeyDown(GLFW_KEY_ENTER) && FUCK(0) == 0)
+			{
+				eCurrentState = eGAMEPLAY;
+				CreateEnemies();
+			}
+	DrawSprite(iArcadeMarquee);
+};
+
+void UpdateGameState()				//UPDATING THE GAME STATE
+{
+	player.Move(fDeltaT, 100);
+	MoveSprite(player.iSpriteID, player.x, player.y);
+	DrawSprite(player.iSpriteID);
+	bool EnemyTurn = false;
+	for (int i = 0; i < 15; i++)
+	{
+		if (Aliens[i].SwitchCheck())
+			EnemyTurn = true;
+	}
+	for (int i = 0; i < 15; i++)
+	{
+		if (EnemyTurn)
+		{
+			Aliens[i].SetLoc(Aliens[i].GetX(), Aliens[i].GetY() - 10);
+			Aliens[i].iDirection *= -1.06;
+		}
+		Aliens[i].GoForward(fDeltaT);
+		MoveSprite(Aliens[i].iSpriteID, Aliens[i].GetX(), Aliens[i].GetY());
+		DrawSprite(Aliens[i].iSpriteID);
+	}
+
+	DrawString("SCORE < 1 >", iScreenWidth * 0.025f, iScreenHeight - 2);
+	DrawString("HI-SCORE", iScreenWidth * 0.4f, iScreenHeight - 2);
+	DrawString("SCORE < 2 >", iScreenWidth * 0.75f, iScreenHeight - 2);
+	DrawString("3", iScreenWidth * 0.025f, .05f * iScreenHeight);
+	DrawString("CREDIT 00", iScreenWidth * .75f, 0.05f * iScreenHeight);
+	DrawLine(0, iScreenHeight * .075f, iScreenWidth, iScreenHeight * .075f, SColour(0x00, 0xFC, 0x00, 0xFF));
+	DrawLine(0, iScreenHeight * .075f - 1, iScreenWidth, iScreenHeight * .075f - 1, SColour(0x00, 0xFC, 0x00, 0xFF));
+	DrawLine(0, iScreenHeight * .075f - 2, iScreenWidth, iScreenHeight * .075f - 2, SColour(0x00, 0xFC, 0x00, 0xFF));
+	for (int a_i = 0; a_i < 780; a_i = a_i + 2)
+	{
+		DrawLine(0, a_i, iScreenWidth, a_i, SColour(0x00, 0x00, 0x00, 0xFF)); //SCANLINES
+	}
+	for (int i = 0; i < 15; i++)
+	{
+		if (CheckBoxBox(player.GetBox2D(), Aliens[i].GetBox2D()))
+			eCurrentState = eEND;
+	}
+};
